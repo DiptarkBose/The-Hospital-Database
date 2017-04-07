708a053dd6f5f72bea7f2f59a824fbd92775b9cb
Diptark Bose <diptark.bose@gmail.com>
2017-04-07 23:02:33 +0530

    Add files via upload

---
+++ Final Submission/Guest Screen/PatientInfo.java
@@ -0,0 +1,117 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*; 
+import java.applet.*;
+import java.sql.*;
+
+public class PatientInfo {
+    Connection con;
+    Statement st;
+    ResultSet rs;
+    String answer="";
+
+    public static void main(String[] args) {
+        new PatientInfo();
+    }
+    
+    public void connect(int id) {
+        
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","dip");
+            st = (Statement) con.createStatement();
+            ResultSet resultSet = st.executeQuery("SELECT * from Patient where PatientID="+id);
+            ResultSetMetaData rsmd = resultSet.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) answer=answer+("\n  ");
+                    String columnValue = resultSet.getString(i);
+                    //System.out.print(rsmd.getColumnName(i)+ ": " +columnValue);
+                    answer=answer+(rsmd.getColumnName(i)+ ": " +columnValue+"       ");
+                }
+                answer=answer+("\n\n");
+            }
+        } catch (Exception ex) {
+        }
+        
+
+    }
+
+    public PatientInfo() {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                }
+                
+                
+                JFrame frame = new JFrame("Patient Information Portal");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new BorderLayout());
+                frame.add(new TestPane());
+                frame.pack();
+                frame.setSize(1000,300);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
+
+    public class TestPane extends JPanel {
+
+        public TestPane() {
+            setLayout(new GridBagLayout());
+            final GridBagConstraints gbc = new GridBagConstraints();
+            gbc.gridx = 1;
+            gbc.gridy = 0;
+            gbc.insets = new Insets(2, 2, 2, 2);
+            
+            
+            add(new JLabel("Enter Patient ID for accessing the records"), gbc);
+            gbc.gridx = 0;
+            gbc.gridy++;
+            add(new JLabel("Patient ID"), gbc);
+            gbc.gridx++;
+            //add(new JLabel("Or"), gbc);
+            //gbc.gridx++;
+            //add(new JLabel("Doctor ID"), gbc);
+
+            gbc.gridx = 0;
+            gbc.gridy++;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            final JTextField f1=new JTextField(10);
+            add(f1, gbc);
+            gbc.gridx++;
+            //add(new JTextField(10), gbc);
+
+            
+            //gbc.gridy++;
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.gridwidth = 2;
+            JButton b1=new JButton("Click");
+            add(b1, gbc);
+            gbc.gridy++;
+            gbc.gridx=0;
+            gbc.gridy++;
+            add(new JLabel("Required Patient Information:"), gbc);
+            gbc.gridy++;
+            gbc.gridy++;
+            gbc.gridx=0;
+            b1.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e)
+                {
+                    int id=Integer.parseInt(f1.getText());
+                    connect(id);
+                    add(new JLabel(answer), gbc);
+                }
+        });
+
+        }
+
+    }
+
+}
